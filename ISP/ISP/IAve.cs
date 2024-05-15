8106838aa84d6606638cedbbeca391e7fb503b63
Miguel Eve <mevealcantara@gmail.com>
2024-05-14 20:25:21 -0400

    Agrega la implementacion de la segregacion de interfaz

---
+++ ISP/ISP/IAve.cs
@@ -0,0 +1,19 @@
+﻿namespace ISP
+{
+    public interface IAve
+    {
+        void Volar();
+        void Cantar();
+        void PonerHuevos();
+    }
+
+    public interface INadar {
+
+        void Nadar();
+    }
+
+    public interface ICazar {
+
+        void Cazar();
+    }
+}
---
+++ ISP/ISP_Realista/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿namespace ISP_Realista
+{
+    public interface IVolador
+    {
+        void Volar();
+    }
+
+    public interface ICantador
+    {
+        void Cantar();
+    }
+    public interface IPonedorHuevos
+    {
+        void PonerHuevos();
+    }
+
+    public interface INadador
+    {
+        void Nadar();
+    }
+    public interface ICazador
+    {
+        void Cazar();
+    }
+}
